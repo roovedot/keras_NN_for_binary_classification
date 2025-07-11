f0bf53d0cc3fa805752300408a6a3654477223ee
Iker Álamo Gómez de Segura <ikeralamo2004@gmail.com>
2025-07-11 20:18:44 +0200

    model_v3 TODO

--- model_v3.py
+++ model_v3.py
@@ -1,2 +1,4 @@
 #TODO: use kfold and crossValidation instead of train_test_split
+#TODO: use class_weight in model.fit to combat class inbalance
+#TODO: EarlyStopping + checkpoint
 #TODO: try kerastuner's hyperband on model_v2 to try to find a more optimised model
